9cbc2f11cb9073baa26ada240a7f4598b9c6cea0
PetrTech <55279432+PetrTech@users.noreply.github.com>
2026-02-22 15:43:15 +0100

    Fix cross-server communication

--- src/config/captcha.ts
+++ src/config/captcha.ts
@@ -1,4 +1,4 @@
 export default {
-    cost: 5,
-    ttl: 120,
+    cost: 5,  // 5 default & recommended - the maximum amount of digits the secret can have, exponential solve time increase
+    ttl: 120, // maximum lifetime of a generated challenge
 }
--- src/middleware/internal.ts
+++ src/middleware/internal.ts
@@ -1,8 +1,11 @@
 import { NextFunction, Request, Response } from "express";
+import config from "../config";
 
 export const internal = (req: Request, res: Response, next: NextFunction): any => {
-    const chiSecret = req.headers['X-Chi-Secret'];
-    if (chiSecret !== process.env.CHI_INTERNAL_KEY) {
+    const chiSecret = req.headers['x-chi-secret'];
+    if (chiSecret !== config.cross_server_secret_key) {
+        console.log(config.cross_server_secret_key);
+        console.log(chiSecret);
         return res.status(403).json({ error: "Forbidden" });
     }
     next();
